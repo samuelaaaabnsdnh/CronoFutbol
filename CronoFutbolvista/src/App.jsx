5c98a7e2e39115f2e4796e027b753ab74b06739c
alejandrodiaz0401-hash <alejandrodiaz.0401@gmail.com>
2026-06-27 11:47:22 -0500

    login y dasboard

--- CronoFutbolvista/src/App.jsx
+++ CronoFutbolvista/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Login from './Login'
 
 function App() {
   const [count, setCount] = useState(0)
